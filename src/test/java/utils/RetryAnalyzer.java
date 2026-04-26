98361155218ee1d7928c38f253db096bdd79806a
Vaishnavip100 <149354824+Vaishnavip100@users.noreply.github.com>
2026-04-26 21:03:03 +0530

    Updated

--- src/test/java/utils/RetryAnalyzer.java
+++ src/test/java/utils/RetryAnalyzer.java
@@ -4,18 +4,15 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-
-    private int count = 0;
-    private static final int maxRetry = 2; // number of retries
+    private int count=0;
+    private static final int maxRetry=2; 
 
     @Override
     public boolean retry(ITestResult result) {
-
         if (count < maxRetry) {
             count++;
-            return true; // retry test
+            return true;
         }
-
-        return false; // stop retrying
+        return false;
     }
 }
